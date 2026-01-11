142137b3674ec89588db45e57fe7591a49231f79
PrashanChamara <prashanchamara.a@gmail.com>
2026-01-11 16:41:54 +0400

    Initial game launch with PWA

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'neon-zen-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event (Offline Support)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
