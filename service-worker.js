5786062ddc5fdac1105c4fda848ea66bf2be6259
PrashanChamara <prashanchamara.a@gmail.com>
2026-01-12 21:08:05 +0400

    New Game Change

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,21 @@
-const CACHE_NAME = 'neon-zen-v1';
+const CACHE_NAME = 'neon-zen-flow-v1';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './css/style.css',
+  './js/main.js',
+  './js/game-engine.js',
+  './js/audio.js',
+  './js/ui.js',
+  './assets/images/logo.jpeg',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// Install Event
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Fetch Event (Offline Support)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
